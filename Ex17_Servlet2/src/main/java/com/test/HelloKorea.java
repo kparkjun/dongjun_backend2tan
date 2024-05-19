aba961c4f2266bf17701bbf626cde6c7e08c5c1d
박준호 <aparkjun@naver.com>
2024-05-19 10:20:22 +0900

    ex17_servlet2

---
+++ Ex17_Servlet2/src/main/java/com/test/HelloKorea.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(description="헬로우코리아",urlPatterns= {"/hello","/korea.do"})
+public class HelloKorea extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException{
+		System.out.println("Hello, Korea~ !!");
+	}
+
+}
